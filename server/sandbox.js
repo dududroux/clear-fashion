b6ae224dfd2f6502e8a8b74cd556df2909c3cb91
dududroux <augustin.vendroux@edu.devinci.fr>
2022-02-03 16:16:05 +0100

    Adresse Paris

--- server/sandbox.js
+++ server/sandbox.js
@@ -1,11 +1,13 @@
 /* eslint-disable no-console, no-process-exit */
 const dedicatedbrand = require('./sites/dedicatedbrand');
-
-async function sandbox (eshop = 'https://www.dedicatedbrand.com/en/men/news') {
+const adresseParis = require('./sites/adresseparis')
+//https://adresse.paris/630-toute-la-collection
+//https://www.dedicatedbrand.com/en/men/news
+async function sandbox (eshop = 'https://adresse.paris/630-toute-la-collection') {
   try {
     console.log(`🕵️‍♀️  browsing ${eshop} source`);
 
-    const products = await dedicatedbrand.scrape(eshop);
+    const products = await adresseParis.scrape(eshop);
 
     console.log(products);
     console.log('done');
