b0eb482237d4336d16d6a9cf33bbc3de0c1f91cc
dududroux <augustin.vendroux@edu.devinci.fr>
2022-03-21 13:42:35 +0100

    api links

--- client/v2/portfolio.js
+++ client/v2/portfolio.js
@@ -60,7 +60,7 @@ const setCurrentProducts = ({result, meta}) => {
 const fetchProducts = async (page = 1, size = 12) => {
   try {
     const response = await fetch(
-      `https://clear-fashion-api.vercel.app?page=${page}&size=${size}`
+      `https://clear-fashion-two-zeta.vercel.app/Products?page=${page}&size=${size}`
     );
     const body = await response.json();
     var groupbyBrand = body.data.result.reduce(function(groups, item) {
